3664b4a109673003013a0fbdbf273a44b6f7121e
Litvinov Sergey <slitvinov@gmail.com>
2013-12-09 20:47:06 +0100

    Add R to the post.sh

--- mcf/collidal_3d_config/C0123_F0/post.sh
+++ mcf/collidal_3d_config/C0123_F0/post.sh
@@ -4,6 +4,9 @@ set -e
 set -u
 
 for c in $(ls colloid/mcf*); do
-    awk '{print $1, $2, $3}' ${c}
+    awk '{print $1, $2, $3, 1.0}' ${c}
     printf "\n"
 done > punto.dat
+
+
+# punto -bg white -r -z 1:2:3:4 punto.dat 

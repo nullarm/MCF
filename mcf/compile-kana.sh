acf58b4bdd1031a4539e1e065bf6a5e05bf6d479
Litvinov Sergey <slitvinov@gmail.com>
2013-12-09 18:04:47 +0100

    Add a script to compile on kana

---
+++ mcf/compile-kana.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set +e
+set +u
+
+PREFIX=/scratch/prefix-mcf/
+
+./configure --prefix=${PREFIX} FC=gfortran MPIFC=${PREFIX}/bin/mpif90 \
+    LDFLAGS=-L${PREFIX}/lib/ FCFLAGS="-I${PREFIX}/include -g" \
+    MAKEDEPF90=${PREFIX}/bin/makedepf90 
+
+make -j 8

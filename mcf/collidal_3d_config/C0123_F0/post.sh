24658df34bb9d18fa8efd4c1cb6b644dd4a29012
Litvinov Sergey <slitvinov@gmail.com>
2013-12-09 20:53:55 +0100

    Remove run.sh, update post.sh

--- mcf/collidal_3d_config/C0123_F0/post.sh
+++ mcf/collidal_3d_config/C0123_F0/post.sh
@@ -3,13 +3,14 @@
 set -e
 set -u
 
-for d in M4_COLB; do
-    echo $d
+for d in $(ls -d COLB*); do
+    cd ${d}
+    for c in $(ls colloid/mcf*); do
+	awk '{print $1, $2, $3, 1.0}' ${c}
+	printf "\n"
+    done > punto.dat
+    cd ..
 done
-for c in $(ls colloid/mcf*); do
-    awk '{print $1, $2, $3, 1.0}' ${c}
-    printf "\n"
-done > punto.dat
 
 
 # punto -bg white -r -z 1:2:3:4 punto.dat 
--- mcf/collidal_3d_config/C0123_F0/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-
-mkdir -p particles boundary colloid
-mpirun -np 8 ../../src/mcf
-

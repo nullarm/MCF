1c1dd389fcec41e875d61923b5527bf7bd6ab331
Litvinov Sergey <slitvinov@gmail.com>
2013-12-10 00:03:59 +0100

    Turn output for punto

--- mcf/collidal_3d_config/C0123_F0/post.sh
+++ mcf/collidal_3d_config/C0123_F0/post.sh
@@ -6,11 +6,11 @@ set -u
 for d in $(ls -d COLB*); do
     cd ${d}
     for c in $(ls colloid/mcf*); do
-	awk '{print $1, $2, $3, 1.0}' ${c}
+	awk '{print $1, $3, $2, 1.0}' ${c}
 	printf "\n"
     done > punto.dat
     cd ..
 done
 
 
-# punto -bg white -r -z 1:2:3:4 punto.dat 
+# punto -bg white -r -z 1:2:3:4 -B 0:0:0:8:8:8 punto.dat

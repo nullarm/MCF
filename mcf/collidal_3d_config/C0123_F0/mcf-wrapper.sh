0f74cb998ddc613225b3c3584282e5d27431ce60
Litvinov Sergey <slitvinov@gmail.com>
2013-12-09 20:46:51 +0100

    Add a first version of the mcf-wrapper

---
+++ mcf/collidal_3d_config/C0123_F0/mcf-wrapper.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function githead() {
+    git --git-dir=${src}/.git --work-tree=${src} rev-parse HEAD
+}
+
+function cpreplace() {
+    local varfile=$1
+    local dname=$2
+    mkdir -p ${dname}/particles ${dname}/boundary ${dname}/colloid
+    shift 2
+    for f in $*; do
+	./vars.awk ${varfile} ${f} > ${dname}/${f}
+    done
+    cp ${varfile} ${dname}/vars.mcf
+}
+
+function var2dirname() {
+    awk -v FS="=" -v ORS="" 'NF{$1=$1; gsub(/ /, ""); print}'  $1
+}
+
+mcf=/scratch/work/MCF/mcf/src/mcf
+src=/scratch/work/MCF/
+
+for b in $(seq 4.5 0.25 6.0); do
+    echo "M4_COLB=$b" > vars.mcf.${b}
+    dname=$(var2dirname vars.mcf.${b})
+    cpreplace vars.mcf.${b} ${dname} ctrl.mcf  io_config.mcf  physics_config.mcf
+    cd ${dname}
+    githead > git.commit.id
+    mpirun -np 1 ./mcf &
+    cd ../
+done
+

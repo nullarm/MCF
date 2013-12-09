787198821ab238761aa64d2e208b725684347c05
Litvinov Sergey <slitvinov@gmail.com>
2013-12-09 23:22:12 +0100

    Compile with gfortran of supermuc

--- ppm/compile-supmuc.sh
+++ ppm/compile-supmuc.sh
@@ -3,13 +3,16 @@
 . /etc/profile
 . /etc/profile.d/modules.sh
 
+set -e
+set -u
+
 module unload mpi.ibm
-module load mpi.intel
+module load mpi.mpich2/1.4/gcc
+fortrancomp=gfortran
+suffix=4.4.5
 
-module unload fortran
-module load fortran/intel/11.1
-
-make PREFIX=/home/hlrb2/pr32ma/lu64yuz3/MCF/ppm/local_intelmpi_ifort111_o2_g \
-     BUILD_MAKE_FLAGS=-j16 FCFLAGS='-g -O2' \
-     WRKDIR=/home/hlrb2/pr32ma/lu64yuz3/MCF/ppm/work_intelmpi_ifort111_o2_g \
+make MPIFC=mpif90 FC=mpif90 CC=mpicc CXX=mpicxx \
+     PREFIX=${HOME}/mcf-${fortrancomp}${suffix} \
+     BUILD_MAKE_FLAGS=-j16 \
+     WRKDIR=${PWD}/work-${fortrancomp}${suffix} \
      install_all_but_mpi

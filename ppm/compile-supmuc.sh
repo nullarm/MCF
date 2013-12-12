f11c86174d8d0156ae6735968e038393c8eab099
Litvinov Sergey <slitvinov@gmail.com>
2013-12-12 13:01:39 +0100

    Switch to a default intel compiler on supermuc

--- ppm/compile-supmuc.sh
+++ ppm/compile-supmuc.sh
@@ -7,11 +7,16 @@ set -e
 set -u
 
 module unload mpi.ibm
-module load mpi.mpich2/1.4/gcc
-fortrancomp=gfortran
-suffix=4.4.5
+module load mpi.intel
 
-make MPIFC=mpif90 FC=mpif90 CC=mpicc CXX=mpicxx \
+module unload fortran
+module load fortran/intel
+
+fortrancomp=ifort
+suffix=
+CXX=mpiCC
+
+make MPIFC=mpif90 FC=mpif90 CC=mpicc CXX=${CXX} \
      PREFIX=${HOME}/mcf-${fortrancomp}${suffix} \
      BUILD_MAKE_FLAGS=-j16 \
      WRKDIR=${PWD}/work-${fortrancomp}${suffix} \

ee54abe9cf9a00d157d955e42843ef4d89ad03a1
Litvinov Sergey <slitvinov@gmail.com>
2014-03-24 14:34:49 +0100

    Transfer colloida files

--- mcf/collidal_3d_config/suspension/transfer.sh
+++ mcf/collidal_3d_config/suspension/transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rsync -av -e '/home/litvinov/bin/chain-ssh -t litvinov@yoko ssh' \
-    lu79buz2@supermuc.lrz.de:/gpfs/scratch/pr32ma/lu79buz2/C* supermuc-data/
+    lu79buz2@supermuc.lrz.de:/gpfs/scratch/pr32ma/lu79buz2/N* supermuc-data/
 

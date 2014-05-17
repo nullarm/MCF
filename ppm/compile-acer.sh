3b55825eb16033ba0973801d920b211a6ebbf0f4
LItvinov Sergey <Litvinov Sergey>
2014-05-17 11:00:33 +0200

    Add acer compilation option

--- ppm/compile-acer.sh
+++ ppm/compile-acer.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-PREFIX=${HOME}/prefix-ifort-mcf
+PREFIX=${HOME}/prefix-gfortran-mcf
 
 make PREFIX=${PREFIX} CXX=g++ CC=gcc FC=${PREFIX}/bin/mpif90 MPIFC=${PREFIX}/bin/mpif90 \
      BUILD_MAKE_FLAGS=-j1 FCFLAGS='-g -O2' \
-     WRKDIR=work-ifort \
+     WRKDIR=work-gfortran \
      install_ppm
